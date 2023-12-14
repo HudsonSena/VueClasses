9ce373a6cab5f24db1a064c66ea5bd3a5e348d1a
Hudson Sena <107481318+HudsonSena@users.noreply.github.com>
2023-12-14 17:01:00 -0300

    update install react vite and tailwindcss

---
+++ TailwindClass/tailwind-class/src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="container h-screen w-screen mx-auto flex items-center justify-center">
+      Hello World
+    </div>
+    
+  )
+}
+
+export default App
